187323b54923a89c2f3fb94b955f5adf6836b67b
Praveen Singh <pvnsingh05@gmail.com>
2024-04-13 19:57:55 +0530

    Added env variables

--- server.ts
+++ server.ts
@@ -1,1 +1,12 @@
-console.log("Welcome to e-book api");
+import app from "./src/app";
+import { config } from "./src/config/config";
+
+const startServer = () => {
+  const PORT = config.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+};
+
+startServer();
