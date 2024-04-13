568a88c65a99d6bb5cbf7ae78e489e12e59f6cf3
Praveen Singh <pvnsingh05@gmail.com>
2024-04-13 20:21:45 +0530

    Database connection setup

--- server.ts
+++ server.ts
@@ -1,11 +1,13 @@
 import app from "./src/app";
 import { config } from "./src/config/config";
+import connectDB from "./src/config/db";
 
 const startServer = () => {
   const PORT = config.PORT || 3000;
 
   app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
+    connectDB();
   });
 };
 
--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 const _config = {
   PORT: process.env.PORT,
+  MONGODB_URI: process.env.MONGODB_URI,
 };
 
 export const config = Object.freeze(_config);
